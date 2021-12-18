dd5c72c2c00c8fc2406945a396a58a284e5bb577
OlegTch <Olgard984@gmail.com>
2021-12-18 21:28:04 +0200

    add images gallery

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,1 +1,14 @@
-// <Button>
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+const Button = ({ handleButton }) => {
+  return (
+    <button className={styles.button} type="button" onClick={handleButton}>
+      Load more...
+    </button>
+  );
+};
+
+Button.propTypes = { handleButton: PropTypes.func.isRequired };
+
+export default Button;
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,5 +1,24 @@
-// <ImageGalleryItem>
+import PropTypes from "prop-types";
+import styles from "./ImageGalleryItem.module.css";
 
-/* <li class="gallery-item">
-  <img src="" alt="" />
-</li>; */
+const ImageGalleryItem = ({ id, image, tags, onImageClick }) => {
+  return (
+    <li className={styles.imageGalleryItem} onClick={onImageClick}>
+      <img
+        className={styles.imageGalleryItemImage}
+        id={id}
+        src={image}
+        alt={tags}
+      />
+    </li>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  onImageClick: PropTypes.func.isRequired,
+};
+
+export default ImageGalleryItem;
